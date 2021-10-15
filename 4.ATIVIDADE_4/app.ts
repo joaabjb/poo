c57355be2ff36a4c6fc5f7b47594c49c267d46e6
Joaab Sousa <48816108+joaabjb@users.noreply.github.com>
2021-10-14 22:37:25 -0300

    Add files via upload

---
+++ 4.ATIVIDADE_4/app.ts
@@ -0,0 +1,21 @@
+
+//ATIVIDADE_4
+
+
+//Questão_1:
+var ehPar = (n) => n%2 == 0;
+
+
+//Questão_2:
+var ola = () => console.log("Olá");
+
+
+//Questão_3:
+const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+var ehPar = (n) => n%2 == 0;
+var arrayPares = array.filter(ehPar)
+for (let n of arrayPares){
+    console.log(n)
+}
+
+
